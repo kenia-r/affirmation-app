6dccf6211e5d74aecb286edebd2e51b36b3c0b50
Kenia Rodriguez <75282664+kenia-r@users.noreply.github.com>
2021-08-21 16:24:32 -0400

    Affirmation api (#2)
    * comp set up
    
    * renders quote

---
+++ ubelong/src/Components/Affirmations.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const Affirmations = () => {
+  const [quote, setQuote] = useState([]);
+
+  const fetchQuote = async () => {
+    try {
+      const ll = fetch(
+        "https://cors-anywhere.herokuapp.com/https://www.affirmations.dev"
+      )
+        .then((res) => res.json())
+        .then(res2 => setQuote(res2));
+    } catch (error) {
+      console.log("hitting this catch", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+ const {affirmation} = quote
+
+  return (
+    <div>
+      <h1>Affirmations quote</h1>
+      <h2>{affirmation}</h2>
+    </div>
+  );
+};
+
+export default Affirmations;
